d15a9ee5ad302efd615adb405274f4bdf5ce0249
Steven Difelippo <matrixdriver04@gmail.com>
2017-07-25 20:47:50 -0400

    update

--- directory.js
+++ directory.js
@@ -3,29 +3,32 @@ const mustacheExpress = require('mustache-express');
 const robotMembers = require('./data.js');
 const app = express();
 
-app.use(express.static('public'))
-
 app.engine('mustache', mustacheExpress());
 
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
 
+// app.get('/solorobots', function(request, response){
+//   response.render('solorobots', { data: robotMembers.users });
+// });
+//
+// app.get('/robotdirectory', function(request, response){
+//   response.render('robotdirectory', { data: robotMembers.users });
+// });
 
-app.get('/index', function(request, response) {
-  response.render('index', {
-    robots: robot.users
-  });
-});
+// app.get('/robot_data/:users', function(request, response){
+//   let index = parseInt(request.params.users);
+//   response.render('staff_member', staffMembers[index]);
+// });
 
-app.get('/details', function(request, response) {
-  response.render('details', {
-    robots: robot.users
-  });
-});
+// app.get('/robot_data/:name', function(request, response){
+//   let person = robotMembers.find(function(member) {
+//     return member.name.toLowerCase() === request.params.name;
+//   })
+//   response.render('robotMembers', person);
+// });
 
-app.get('/index/:id', function(request, response) {
-  let person = robot.find(function(member) {
-    return member.name.toLowerCase() === request.params.id;
-  });
-  response.render('index', person)
-});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
