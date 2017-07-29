7d083df7bb814d696cc8fbf56a62acaf7f6e174f
Steven Difelippo <matrixdriver04@gmail.com>
2017-07-29 18:57:56 -0400

    almostfinished

--- directory.js
+++ directory.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
-const robotMembers =require('./robotdata.json');
+const robot = require('./robotdata.json');
 const app = express();
+
 
 app.engine('mustache', mustacheExpress());
 
@@ -9,23 +10,17 @@ app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
 
 app.use(express.static('public'));
-app.get('/solorobots', function(request, response){
-  response.render('solorobots', { robotdata: robotMembers.users });
-});
-
-app.get('/robotdirectory', function(request, response){
-  response.render('robotdirectory', { robotdata: robotMembers.users });
-});
-
-
-app.get('/solorobots/:name', function(request, response){
-  let person = robotMembers.find(function(member) {
-    return member.name.toLowerCase() === request.params.name;
-  })
-  response.render('robot_data', person);
-});
-
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.get('/robotdirectory', function(request, response) {
+  response.render('robotdirectory', {staff: robot.users})
 })
+
+
+app.get('/:username', function(request, response) {
+  const user = robot.users.find(function(user){return user.username === request.params.username});
+  response.render('solorobots', {staff: user});
+})
+
+
+app.listen(3000, function() {
+  console.log('I\'m Alive!!!')
+});
